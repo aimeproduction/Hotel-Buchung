18b72dec557668212caefd8e313fd3691b6f7f14
aimeproduction <m.nangue@gmail.com>
2023-01-18 14:06:15 +0100

    Refactoring done

---
+++ src/app/app.module.ts
@@ -0,0 +1,62 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListBuchungComponent } from './components/list-buchung/list-buchung.component';
+import { NeueBuchungComponent } from './components/neue-buchung/neue-buchung.component';
+import { EditBuchungComponent } from './components/edit-buchung/edit-buchung.component';
+import { DeleteBuchungComponent } from './components/delete-buchung/delete-buchung.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from "@angular/material/input";
+import {MatOptionModule} from "@angular/material/core";
+import {MatSelectModule} from "@angular/material/select";
+import {MatIconModule} from "@angular/material/icon";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatButtonModule} from "@angular/material/button";
+import {MatDividerModule} from "@angular/material/divider";
+import {HttpClientModule} from "@angular/common/http";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {Ng2SearchPipeModule} from "ng2-search-filter";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatTooltipModule} from "@angular/material/tooltip";
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListBuchungComponent,
+    NeueBuchungComponent,
+    EditBuchungComponent,
+    DeleteBuchungComponent,
+    LoginComponent,
+    NavbarComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatDividerModule,
+    HttpClientModule,
+    MatSnackBarModule,
+    Ng2SearchPipeModule,
+    MatDialogModule,
+    MatTooltipModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/list-buchung/list-buchung.component.ts
@@ -0,0 +1,68 @@
+import { Component } from '@angular/core';
+import {catchError, Observable, throwError} from "rxjs";
+import {Customer} from "../../models/customer";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {MatDialog} from "@angular/material/dialog";
+import {HotelServiceService} from "../../service/hotel-service.service";
+import {DeleteBuchungComponent} from "../delete-buchung/delete-buchung.component";
+import {EditBuchungComponent} from "../edit-buchung/edit-buchung.component";
+
+@Component({
+  selector: 'app-list-buchung',
+  templateUrl: './list-buchung.component.html',
+  styleUrls: ['./list-buchung.component.css']
+})
+export class ListBuchungComponent {
+  index: number =0;
+  customer_id: number = 0;
+  data$!: Observable<Customer[]>;
+  search!: string;
+  public form_search!: FormGroup;
+  errorObject = '';
+
+  constructor(private _snackBar: MatSnackBar,
+              public dialog: MatDialog, private service: HotelServiceService, private fb: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.refresh();
+
+
+    this.form_search = this.fb.group({
+      search: [''],
+    });
+  }
+
+  delete_student( id: number, index: number) {
+    this.index = index;
+    this.customer_id = id;
+    this.dialog.open(DeleteBuchungComponent, {
+      width: '500px', height: '500px',
+      data: {customer_id: this.customer_id, index: this.index}
+    }).afterClosed().subscribe(result => {
+      this.refresh();
+    });
+  }
+
+
+  update_student(index: number) {
+    this.customer_id = index;
+    this.dialog.open(EditBuchungComponent, {
+      width: '700px', height: '650px',
+      data: {customer_id: this.customer_id}
+    }).afterClosed().subscribe(result => {
+      this.refresh();
+    });
+  }
+
+  refresh() {
+    this.errorObject = '';
+    this.data$ = this.service.getAllData().pipe(
+      catchError(err => {
+        this.errorObject = 'Sorry, it was not possible to load the data.';
+        return throwError(err);
+      })
+    );
+  }
+}
